44a482df9f32b6b66545f6655fb09486b6b522b8
Proshanto <proshantodabnath2001@gmail.com>
2024-09-29 09:39:30 -0400

    Fixed account tabs to properly show selected tab

--- frontend/components/ui/RecentTransactions.tsx
+++ frontend/components/ui/RecentTransactions.tsx
@@ -1,5 +1,7 @@
+'use client'
+
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { BankTabItem } from './BankTabItem'
 import BankInfo from './BankInfo'
@@ -11,45 +13,50 @@ const RecentTransactions = ({
     appwriteItemId,
     page = 1
 }: RecentTransactionsProps) => {
-  return (
-    <section className='recent-transactions'>
-        <header className='flex items-center justify-between'>
-            <h2 className='recent-transactions-label'>
-                Recent Transactions
-            </h2>
-            <Link href={`/transaction-history`} className='view-all-btn'>
-                View All
-            </Link>
-        </header>
-        <Tabs defaultValue={appwriteItemId} className="w-full">
-            <TabsList className='recent-transactions-tablist'>
-                {accounts.map((account) => (
-                    <TabsTrigger key={account.id} value={account.appwriteItemId}>
-                        <BankTabItem 
-                            key={account.id}
+    const [activeTab, setActiveTab] = useState(appwriteItemId); // useState to dynamically update id
+    const handleTabChange = (newId: React.SetStateAction<string>) => {
+      setActiveTab(newId);
+    };
+
+    return (
+        <section className='recent-transactions'>
+            <header className='flex items-center justify-between'>
+                <h2 className='recent-transactions-label'>
+                    Recent Transactions
+                </h2>
+                <Link href={`/transaction-history`} className='view-all-btn'>
+                    View All
+                </Link>
+            </header>
+            <Tabs value={activeTab} onValueChange={handleTabChange} className="w-full">
+                <TabsList className='recent-transactions-tablist'>
+                    {accounts.map((account) => (
+                        <TabsTrigger key={account.id} value={account.appwriteItemId}>
+                            <BankTabItem 
+                                key={account.id}
+                                account={account}
+                                appwriteItemId={activeTab}
+                            />
+                        </TabsTrigger>
+                    ))}
+                </TabsList>
+                {accounts.map((account: Account) => (
+                    <TabsContent
+                        value={account.appwriteItemId}
+                        key={account.id}
+                        className='space-y-4'
+                    >
+                        <BankInfo 
                             account={account}
-                            appwriteItemId={appwriteItemId}
+                            appwriteItemId={activeTab}
+                            type='full'
                         />
-                    </TabsTrigger>
+                        <TransactionsTable transactions={transactions} />
+                    </TabsContent>
                 ))}
-            </TabsList>
-            {accounts.map((account: Account) => (
-                <TabsContent
-                    value={account.appwriteItemId}
-                    key={account.id}
-                    className='space-y-4'
-                >
-                    <BankInfo 
-                        account={account}
-                        appwriteItemId={appwriteItemId}
-                        type='full'
-                    />
-                    <TransactionsTable transactions={transactions} />
-                </TabsContent>
-            ))}
-        </Tabs>
-    </section>
-  )
+            </Tabs>
+        </section>
+    )
 }
 
 export default RecentTransactions
