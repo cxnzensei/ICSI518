2cfe767ce941b121df831958c1c03b798723d678
cxnzensei <nishitpatil3231@gmail.com>
2024-10-01 14:32:40 -0400

    Added exception controller and react toastify for showing server messages.

---
+++ backend/src/main/java/com/icsi518/backend/controllers/ExceptionController.java
@@ -0,0 +1,16 @@
+package com.icsi518.backend.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.icsi518.backend.exceptions.ApplicationException;
+
+@ControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(ApplicationException.class)
+    public ResponseEntity<String> handleApplicationExceptions(ApplicationException e) {
+        return new ResponseEntity<>(e.getMessage(), e.geStatus());
+    }
+}
--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -242,6 +242,6 @@ export const request = (method: string, url: string, data: object = {}) => {
     method: method,
     url: url,
     data: data,
-    // withCredentials: true
+    withCredentials: method !== 'GET' && true // temporary, change it later
   })
 }
