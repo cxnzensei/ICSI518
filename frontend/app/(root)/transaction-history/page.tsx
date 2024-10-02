7ae9a2acefdcad2d1a6346879f9a6897080dacdb
cxnzensei <nishitpatil3231@gmail.com>
2024-10-02 08:53:47 -0400

    "Coldfix"

--- frontend/app/(root)/transaction-history/page.tsx
+++ frontend/app/(root)/transaction-history/page.tsx
@@ -5,12 +5,12 @@ import React from 'react'
 import { getLoggedInUser } from '@/lib/utils';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, Suspense } from 'react';
 import TransactionsTable from '@/components/TransactionsTable';
 import { BankTabItem } from '@/components/BankTabItem';
 import BankInfo from '@/components/BankInfo';
 
-const TransactionHistory = ({ appwriteItemId }: RecentTransactionsProps) => {
+const TransactionHistory = () => {
     const [activeTab, setActiveTab] = useState('1'); // useState to dynamically update id
 
     const filterTransactionsForAccount = (accountId: string) => {
@@ -179,36 +179,38 @@ const TransactionHistory = ({ appwriteItemId }: RecentTransactionsProps) => {
     ];
 
     return (
-        <div className="transactions">
-            <div className="transactions-header">
-                <HeaderBox
-                    type="greeting"
-                    title="Welcome to your Transaction History,"
-                    user={loggedInUser?.firstName || 'Guest'}
-                    subtext="See your bank details and Transactions."
-                />
-            </div>
-            <Tabs value={activeTab} onValueChange={handleTabChange} defaultValue={appwriteItemId} className="w-full">
-                <TabsList className='recent-transactions-tablist'>
-                    {accounts.map((account: Account) => (
-                        <TabsTrigger key={account.id} value={account.appwriteItemId}>
-                            <BankTabItem key={account.id} account={account} appwriteItemId={appwriteItemId} />
-                        </TabsTrigger>
-                    ))}
-                </TabsList>
+        <Suspense fallback={<div>Loading ...</div>}>
+            <div className="transactions">
+                <div className="transactions-header">
+                    <HeaderBox
+                        type="greeting"
+                        title="Welcome to your Transaction History,"
+                        user={loggedInUser?.firstName || 'Guest'}
+                        subtext="See your bank details and Transactions."
+                    />
+                </div>
+                <Tabs value={activeTab} onValueChange={handleTabChange} defaultValue={'1'} className="w-full">
+                    <TabsList className='recent-transactions-tablist'>
+                        {accounts.map((account: Account) => (
+                            <TabsTrigger key={account.id} value={account.appwriteItemId}>
+                                <BankTabItem key={account.id} account={account} appwriteItemId={account.appwriteItemId} />
+                            </TabsTrigger>
+                        ))}
+                    </TabsList>
 
-                {accounts.map((account: Account) => (
-                    <TabsContent
-                        value={account.appwriteItemId}
-                        key={account.id}
-                        className='space-y-4'
-                    >
-                        <BankInfo account={account} appwriteItemId={appwriteItemId} type='full' />
-                        <TransactionsTable transactions={filterTransactionsForAccount(account.id)} />
-                    </TabsContent>
-                ))}
-            </Tabs>
-        </div>
+                    {accounts.map((account: Account) => (
+                        <TabsContent
+                            value={account.appwriteItemId}
+                            key={account.id}
+                            className='space-y-4'
+                        >
+                            <BankInfo account={account} appwriteItemId={account.appwriteItemId} type='full' />
+                            <TransactionsTable transactions={filterTransactionsForAccount(account.id)} />
+                        </TabsContent>
+                    ))}
+                </Tabs>
+            </div>
+        </Suspense>
     )
 }
 
