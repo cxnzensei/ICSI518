6c2adf8b23ecda5cf7e24bb189ed70ce1f792f9b
Nishit Patil <86833783+cxnzensei@users.noreply.github.com>
2024-10-24 22:38:23 -0400

    Update BackendApplication.java
    Add an Admin user on startup

--- backend/src/main/java/com/icsi518/backend/BackendApplication.java
+++ backend/src/main/java/com/icsi518/backend/BackendApplication.java
@@ -1,12 +1,41 @@
 package com.icsi518.backend;
 
+import com.icsi518.backend.enums.Role;
+import com.icsi518.backend.entities.User;
+import com.icsi518.backend.repositories.UserRepository;
+
+import java.util.Optional;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+		return args -> {
+
+			Optional<User> presentUser = userRepository.findByEmailId("team8@albany.edu");
+
+			if (presentUser.isPresent()) {
+				return;
+			}
+
+			User user = new User();
+			user.setEmailId("team8@albany.edu");
+			user.setPassword(passwordEncoder.encode("password"));
+			user.setFirstName("Admin");
+			user.setLastName("User");
+			user.setRole(Role.valueOf("ADMIN"));
+			userRepository.save(user);
+		};
 	}
 }
