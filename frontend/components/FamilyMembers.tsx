5725c9079addca14b2d0f913f455db9be2fe029b
cxnzensei <nishitpatil3231@gmail.com>
2024-10-27 10:55:25 -0400

    feat: added member management accordion on family page

--- frontend/components/FamilyMembers.tsx
+++ frontend/components/FamilyMembers.tsx
@@ -4,6 +4,15 @@ import {
     TableCell,
     TableRow,
 } from "@/components/ui/table"
+
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion"
+
+
 import { FamilyMember, loginResponse } from '@/types';
 import { convertToFamilyMember, request, setLoggedInUser } from '@/lib/utils';
 
@@ -21,7 +30,7 @@ const FamilyMembers: React.FC<FamilyMembers> = ({ members, setMembers, setCreate
         try {
             await request("DELETE", "/api/v1/families/remove-user-from-family", { "userId": id });
             const updatedFamily = members?.filter(member => member.id !== id)
-            if (action === 'DECLINED' || action === 'LEAVE') {
+            if (action === 'DECLINE' || action === 'LEAVE') {
                 console.log("should set to empty")
                 setMembers([])
             } else {
@@ -70,41 +79,61 @@ const FamilyMembers: React.FC<FamilyMembers> = ({ members, setMembers, setCreate
     }
 
     return (
-        <Table>
-            <TableBody>
-                {members?.map((member: FamilyMember) => (
-                    <TableRow key={member.id} className='border'>
-                        <TableCell className='px-2'>{member.firstName} {member.lastName}</TableCell>
-                        <TableCell className='px-2'>{member.emailId}</TableCell>
-                        <TableCell className='px-2'>{member.role}</TableCell>
-                        <TableCell className='px-2'>{member.membershipStatus}</TableCell>
-                        {(member.membershipStatus === 'PENDING' && member.id === user.id) && (
-                            <TableCell className="flex gap-2">
-                                <button onClick={() => acceptInvite(member?.id)} className="bg-green-500 px-4 py-2 rounded-md text-white">Accept</button>
-                                <button onClick={() => removeUserFromFamily(member?.id, 'DECLINE')} className="bg-red-500 px-4 py-2 rounded-md text-white">Decline</button>
-                            </TableCell>
-                        )}
-                        {(user.role === 'ADMIN' && member.membershipStatus === 'ACCEPTED') && (
-                            <TableCell className="flex gap-2">
-                                {member?.id === user.id ? (
-                                    <button onClick={() => removeUserFromFamily(member?.id, 'LEAVE')} className='bg-red-500 px-4 py-2 rounded-md text-white'>Leave</button>
-                                ) : (
-                                    <button onClick={() => removeUserFromFamily(member?.id, 'REMOVE')} className='bg-red-500 px-4 py-2 rounded-md text-white'>Remove</button>
-                                )}
-                                {member.id !== user.id && (
-                                    <button onClick={() => toggleAdmin(member?.id)} className='bg-blue-500 px-4 py-2 rounded-md text-white'>{member.role === 'ADMIN' ? "Remove Admin" : "Add Admin"}</button>
-                                )}
-                            </TableCell>
-                        )}
-                        {(user.role !== 'ADMIN' && user.id === member.id && member.membershipStatus !== 'PENDING') && (
-                            <TableCell>
-                                <button onClick={() => removeUserFromFamily(member?.id, 'LEAVE')} className='bg-red-500 px-4 py-1 rounded-md text-white'>Leave</button>
-                            </TableCell>
-                        )}
-                    </TableRow>
-                ))}
-            </TableBody>
-        </Table>
+        <div>
+            {members?.map((member: FamilyMember) => (
+                <div key={member.id}>
+                    <Accordion type="single" collapsible>
+                        <AccordionItem value="item-1">
+                            <AccordionTrigger>
+                                <div className="text-lg">
+                                    {member.firstName} {member.lastName} <span className="text-sm">{(member.membershipStatus === 'PENDING' && member.id === user.id) && "(Pending Action)"}</span>
+                                </div>
+                            </AccordionTrigger>
+                            <AccordionContent className="flex flex-col gap-2">
+                                <div>Email: {member.emailId}</div>
+                                <div>Role: {member.role}</div>
+                                <div>Membership Status: {member.membershipStatus}</div>
+                                <div>
+                                    {(member.membershipStatus === 'PENDING' && member.id === user.id) && (
+                                        <div className="flex gap-2">
+                                            <button onClick={() => acceptInvite(member?.id)} className="bg-green-500 px-4 py-2 rounded-md text-white">Accept</button>
+                                            <button onClick={() => removeUserFromFamily(member?.id, 'DECLINE')} className="bg-red-500 px-4 py-2 rounded-md text-white">Decline</button>
+                                        </div>
+                                    )}
+                                </div>
+                                <div>
+                                    {(user.role === 'ADMIN' && member.membershipStatus === 'ACCEPTED') && (
+                                        <div className="flex gap-2">
+                                            {member.id !== user.id && (
+                                                <button onClick={() => toggleAdmin(member?.id)} className='bg-blue-500 px-4 py-2 rounded-md text-white'>{member.role === 'ADMIN' ? "Remove Admin" : "Add Admin"}</button>
+                                            )}
+                                            {member?.id === user.id ? (
+                                                <button onClick={() => removeUserFromFamily(member?.id, 'LEAVE')} className='bg-red-500 px-4 py-2 rounded-md text-white'>Leave</button>
+                                            ) : (
+                                                <button onClick={() => removeUserFromFamily(member?.id, 'REMOVE')} className='bg-red-500 px-4 py-2 rounded-md text-white'>Remove</button>
+                                            )}
+                                        </div>
+                                    )}
+                                </div>
+                                <div>
+                                    {(user.role !== 'ADMIN' && user.id === member.id && member.membershipStatus !== 'PENDING') && (
+                                        <div>
+                                            <button onClick={() => removeUserFromFamily(member?.id, 'LEAVE')} className='bg-red-500 px-4 py-1 rounded-md text-white'>Leave</button>
+                                        </div>
+                                    )}
+                                </div>
+                                <div>
+                                    {(user.role === 'ADMIN' && member.role === 'USER') && (
+                                        <div className="italic">Financial Settings (Admin Privilege)</div>
+                                    )}
+                                </div>
+                            </AccordionContent>
+                        </AccordionItem>
+                    </Accordion>
+                </div>
+            ))
+            }
+        </div >
     );
 };
 
