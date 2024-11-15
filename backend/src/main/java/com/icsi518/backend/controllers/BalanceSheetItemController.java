8ed97398e33cee360977e5bdcb4777b4d6055ac4
cxnzensei <nishitpatil3231@gmail.com>
2024-11-15 15:20:15 -0500

    update to BSC

--- backend/src/main/java/com/icsi518/backend/controllers/BalanceSheetItemController.java
+++ backend/src/main/java/com/icsi518/backend/controllers/BalanceSheetItemController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.icsi518.backend.entities.BalanceSheetItem;
-import com.icsi518.backend.exceptions.ApplicationException;
 import com.icsi518.backend.services.BalanceSheetItemService;
 
 @RestController
@@ -40,39 +38,21 @@ public class BalanceSheetItemController {
     @PostMapping(path = "/create/{accountId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<BalanceSheetItem> createBalanceSheetItem(@PathVariable UUID accountId,
             @RequestBody BalanceSheetItem balanceSheetItem) {
-        try {
-            BalanceSheetItem savedItem = balanceSheetItemService.createBalanceSheetItem(accountId, balanceSheetItem);
-            return ResponseEntity.ok(savedItem);
-        } catch (ApplicationException e) {
-            throw new ApplicationException(e.getMessage(), e.getStatus());
-        } catch (Exception e) {
-            throw new ApplicationException(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        BalanceSheetItem savedItem = balanceSheetItemService.createBalanceSheetItem(accountId, balanceSheetItem);
+        return ResponseEntity.ok(savedItem);
     }
 
     @DeleteMapping(path = "/delete/{itemId}")
     public ResponseEntity<Void> deleteBalanceSheetItem(@PathVariable UUID itemId) {
-        try {
-            balanceSheetItemService.deleteBalanceSheetItem(itemId);
-            return ResponseEntity.noContent().build();
-        } catch (ApplicationException e) {
-            throw new ApplicationException(e.getMessage(), e.getStatus());
-        } catch (Exception e) {
-            throw new ApplicationException(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        balanceSheetItemService.deleteBalanceSheetItem(itemId);
+        return ResponseEntity.noContent().build();
     }
 
     @PutMapping(path = "/update/{itemId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<BalanceSheetItem> updateBalanceSheetItem(@PathVariable UUID itemId,
             @RequestBody BalanceSheetItem balanceSheetItemDetails) {
-        try {
-            BalanceSheetItem updatedBalanceSheetItem = balanceSheetItemService.updateBalanceSheetItem(itemId,
-                    balanceSheetItemDetails);
-            return ResponseEntity.ok(updatedBalanceSheetItem);
-        } catch (ApplicationException e) {
-            throw new ApplicationException(e.getMessage(), e.getStatus());
-        } catch (Exception e) {
-            throw new ApplicationException(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        BalanceSheetItem updatedBalanceSheetItem = balanceSheetItemService.updateBalanceSheetItem(itemId,
+                balanceSheetItemDetails);
+        return ResponseEntity.ok(updatedBalanceSheetItem);
     }
 }
